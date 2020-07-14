dc7b8de2d998a7a5daf6d4f12d8fb41e8ebc8ede
guistetter <guilherme_stetter@hotmail.com>
2020-07-14 15:05:03 -0300

    criando pagina home

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/", (req,res) => {
- return res.send("Ola mundo")
+ return res.render("home")
 })
 
 app.listen(port,(err) => {
